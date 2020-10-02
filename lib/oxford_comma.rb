34985caff01ee978f61ac0ff9c17ca8d896011b3
jCode319 <jdjack3@gmail.com>
2020-10-01 21:38:04 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length == 1
+        new_array = array.join(" ")
+    elsif array.length == 2
+        new_array = array.join(" and ")
+    elsif array.length >= 3
+        new_array = "and #{array[-1]}"
+        array.pop
+        array << new_array
+        array.join(", ")
+    end
 end
